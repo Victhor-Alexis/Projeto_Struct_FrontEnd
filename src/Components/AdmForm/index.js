a8ceb6d64ee3ae9ac47732a38477eb58ea453b01
Victhor-Alexis <victhoralexis123@gmail.com>
2021-12-06 14:36:43 -0300

    Ajustes.

--- src/Components/AdmForm/index.js
+++ src/Components/AdmForm/index.js
@@ -6,44 +6,76 @@ import { Container } from './styles'
 const AdmForm = () => {
 
     const {optionCrud, optionModel, displayAdmForm, admFormShow, 
-           selectedItemId, modelForm, formKind, realOptionModel} = useDynimicityContext();
+           selectedItemId, modelForm, formKind, realOptionModel,
+           managementOption} = useDynimicityContext();
 
     const [name, setName] = useState("")
-
-    const fetchValues = async () => {
-        const response = await api.get(`${realOptionModel}/show/${selectedItemId}`)
-        //onsole.log(response.data)
-        setName(response.data.name)
-    }
+    const [price, setPrice] = useState(-1)
+    const [categories, setCategories] = useState([])
+    const [description, setDescription] = useState("")
+    const [image, setImage] = useState("")
 
     useEffect(() => {
+        const fetchValues = async () => {
+            const response = await api.get(`${realOptionModel}/show/${selectedItemId}`)
+            //onsole.log(response.data)
+            setName(response.data.name)
+            
+            if (realOptionModel === 'products') {
+                setPrice(response.data.price)
+                setCategories(response.data.categories)
+                setDescription(response.data.description)
+                setImage(response.data.image_url)
+            }
+        }
+
         if (selectedItemId !== -1)
             fetchValues()
     }, [selectedItemId, optionCrud])
 
     const selectSubmitType = () => {
         if (optionCrud === 'Editar') {
-            updateModel()
+            return updateModel;
         }
     }
 
-    const updateModel = async () => {
+    const updateModel = async (event) => {
         //console.log(realOptionModel, selectedItemId)
-        await api.patch(`${realOptionModel}/update/${selectedItemId}`, {
-            category: {
-                name
-            }
-        }).then((response) => {alert("Categoria editada")})
+
+        let objectParameter = {};
+
+        switch (realOptionModel) {
+            case 'categories':
+                objectParameter = {
+                    category: {
+                        name
+                    }
+                }
+            break;
+
+            case 'products':
+                objectParameter = {
+                    product: {
+                        name,
+                        price,
+                        description
+                    }
+                }
+            break;
+        }
+
+        event.preventDefault();
+        await api.patch(`${realOptionModel}/update/${selectedItemId}`, objectParameter).then((response) => alert(name+" editado(a)!"))
     }
 
     // Existe a opção de fazer um form para cada opção do crud. Ia ficar mais organizado
     // que a forma que fiz aqui, mas teria mais componentes e trechos repetidos.
 
     return (
-        <Container style={{display: displayAdmForm[0], opacity: displayAdmForm[1]}} onSubmit={selectSubmitType}>
+        <Container style={{display: displayAdmForm[0], opacity: displayAdmForm[1]}} onSubmit={selectSubmitType()}>
             <h1>{optionCrud} {optionModel.toLowerCase()}</h1>
 
-            <div className="close" onClick={() => admFormShow(true, -1, "")}>
+            <div className="close" onClick={() => {admFormShow(true, -1, ""); managementOption(optionModel)}}>
                 <svg width="36" height="34" viewBox="0 0 36 34" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <line x1="2.70711" y1="1.29289" x2="34.7071" y2="33.2929" stroke="#161616" strokeWidth="2"/>
                     <line x1="1.29289" y1="33.2929" x2="33.2929" y2="1.29289" stroke="#161616" strokeWidth="2"/>
@@ -57,18 +89,13 @@ const AdmForm = () => {
                 </div>
 
                 <div className="inputWrapper" style={{display: modelForm[0]}}>
-                    <h2>Categoria:</h2>
-                    <input type="text"/>
-                </div>
-
-                <div className="inputWrapper" style={{display: modelForm[0]}}>
                     <h2>Preço:</h2>
-                    <input type="text"/>
+                    <input type="text" value={price} onChange={(e) => {setPrice(e.target.value)}}/>
                 </div>
 
                 <div className="inputWrapper" style={{display: modelForm[0]}}>
                     <h2>Descrição:</h2>
-                    <input type="text"></input>
+                    <input type="text" value={description} onChange={(e) => {setDescription(e.target.value)}}/>
                 </div>
 
                 <div className="inputWrapper" style={{display: modelForm[1]}}>
